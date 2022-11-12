63e2ddb064a555b5d7e5b3b7dc66daf2714ef6b0
Luckidubi <nwodochidubem1@gmail.comgit config --global user.email nwodochidubem1@gmail.comgit config --global user.email nwodochidubem1@gmail.com>
2022-11-11 23:58:38 -0500

    feat: Add Text and input on homepage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,75 @@
+import {
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightAddon,
+} from "@chakra-ui/react";
 const Home = () => {
-    
-  return <></>;
+  return (
+    <>
+      <Box
+        maxW="1221px"
+        w="full"
+        h="459px"
+        left="100px"
+        top="216px"
+        position="absolute"
+      >
+        <Flex direction="row" justify="center" align="center" gap="6.1875em">
+          <Flex
+            direction="column"
+            align="flex-start"
+            gap="3em"
+            maxW="646px"
+            h="400px"
+          >
+            <Box
+              fontFamily="Red Rose"
+              fontStyle="normal"
+              fontWeight="400"
+              fontSize="56px"
+              lineHeight="140%"
+              letterSpacing="-0.02em"
+              color="#434343"
+              order="0"
+            >
+              Rent a Place away from Home in the Metaverse
+            </Box>
+            <Box
+              fontFamily="Red Rose"
+              fontStyle="normal"
+              fontWeight="400"
+              fontSize="24px"
+              lineHeight="35px"
+              color="#434343"
+              order="1"
+            >
+              we provide you access to luxury and affordable houses in the
+              metaverse, get a chance to turn your imagination to reality at
+              your comfort zone
+            </Box>
+            <Box w="618px" h="54px" order="2" flex-grow="0">
+              <InputGroup size="md">
+                <Input w="360px" h="54px" boxSizing="border-box" />
+                <InputRightAddon
+                  bg="linear-gradient(90deg, #A02279 11.45%, #A02279 11.46%)"
+                  borderRadius="0px 8px 8px 0px"
+                  lineHeight="19px"
+                  w="230px"
+                  textAlign="center"
+                  fontSize="16px"
+                  h="54px"
+                  children="search"
+                  color="#FFFFFF"
+                />
+              </InputGroup>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,38 @@
-import { Box, Flex, Image, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Image, Spacer, Button } from "@chakra-ui/react";
 import metaBnb from "/src/assets/MetaBnbIcon.png";
 import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <Box maxW="1240px" w="full" h="48px">
-        <Flex w="full">
+      <Box
+        maxW="1240px"
+        h="48px"
+        top="43px"
+        left={{ lg: "50px", xl: "100px" }}
+        right={{ lg: "50px", xl: "100px" }}
+        position="absolute"
+      >
+        <Flex w="full" position="relative">
           <Box boxShadow="rgba(96,96,96,0.15)">
-            <Image src={metaBnb} alt="metabnb icon" />
+            <Image
+              w={{ md: "140px", lg: "180px", xl: "233px" }}
+              h={{ lg: "26.6", xl: "36.67px" }}
+              src={metaBnb}
+              alt="metabnb icon"
+            />
           </Box>
           <Spacer />
           <Box
             display="flex"
             flexDirection="row"
-            alignItems="center"
-            gap="48px"
+            alignItems="flex-end"
+            gap={{ lg: "38px", xl: "48px" }}
             fontFamily="Red Rose"
             fontStyle="normal"
             fontWeight="400"
-            fontSize="20px"
+            fontSize={{ lg: "18px", xl: "20px" }}
+            color="#434343"
+            
           >
             <li>
               <Link to="/">Home</Link>
@@ -33,7 +47,19 @@ function Header() {
               <Link to="/">Community</Link>
             </li>
           </Box>
-          <Box></Box>
+          <Spacer />
+          <Box>
+            <Button
+              fontFamily="Red Rose"
+              fontStyle="normal"
+              colorScheme=""
+              color="#FFFFFF"
+              borderRadius="10px"
+              bg="linear-gradient(90deg, #A02279 11.45%, #A02279 11.45%)"
+            >
+              Connect wallet
+            </Button>
+          </Box>
         </Flex>
       </Box>
     </>
